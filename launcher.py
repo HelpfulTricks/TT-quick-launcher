b0487797a9ea54dd5ca4e933f37d8998765260a4
TheMaskedMeowth <thebigm223@gmail.com>
2018-11-25 12:29:19 -0800

    v1.1
    Reimplemented TTR launch support, both launchers are now in the same file. You have to run the launcher script as an administrator if you're using it for TTR.

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
-#TTCC/TTR launcher script v1.0.7
+#Toontown Corporate Clash & Toontown Rewritten launcher script v1.1
 #made by TheMaskedMeowth, this super epic script lets you log in at light speeds not previously known to mankind
-#requirements: python 3.7, requests library (just do pip install requests on command prompt after you get python), you gotta put it in your TTCC folder or else it wont work lol
+#requirements: python 3.7, requests library (just do pip install requests on command prompt after you get python), you have to put this file in your game folder
 
 import os
 import sys
@@ -62,6 +62,8 @@ def login():
 					rs = True
 			if game == 'C':
 				startCC(credentials[ord(c) - 48], vb, rs)
+			elif game == 'R':
+				startTTR(credentials[ord(c) - 48], vb, rs)
 				
 def startCC(tc, vb, rs):
 	import requests
@@ -72,15 +74,53 @@ def startCC(tc, vb, rs):
 		os.environ["TT_PLAYCOOKIE"] = r.json()[u'token']
 		os.environ["TT_GAMESERVER"] = "gs.corporateclash.net"
 		if vb:
-			print("Please note that verbose output only works for the last client opened.")
-			clash = subprocess.Popen(args="CorporateClash.exe")
-			sphandler(clash, tc, rs)
+			gw = subprocess.Popen(args="CorporateClash.exe")
+			spHandler(gw, tc, rs)
 		else:
-			clash = subprocess.Popen(args="CorporateClash.exe", creationflags=0x08000000)
+			gw = subprocess.Popen(args="CorporateClash.exe", creationflags=0x08000000)
 		return True
 	else:
 		print("Login failed with error code " + str(r.json()[u'reason']) + ". (" + str(r.json()[u'friendlyreason']) + ")")
 		print("Press any key to restart...")
+		junk = msvcrt.getch()
+		os.system("cls")
+		login()
+		return False
+		
+def startTTR(tc, vb, rs):
+	import requests
+	import time
+	url = ('https://www.toontownrewritten.com/api/login?format=json')
+	r = requests.post(url, json=tc)
+	response = r.json()
+	success = response[u'success']
+	if success == "delayed":
+		queueToken = {'queueToken': response[u'queueToken']}
+		delayed = True
+		eta = response[u'eta']
+		while delayed:
+			print("You've been put into the queue. ETA: " + eta + " seconds", end='\r')
+			time.sleep(5)
+			r = requests.post(url, json=queueToken)
+			response = r.json()
+			success = response[u'success']
+			if success == "true":
+				delayed = False
+			else:
+				eta = response[u'eta']
+	if success == "true":
+		print("You've been put into the queue. ETA: 0 seconds")
+		print("Welcome back to Toontown, " + tc[u'username'] + "!")
+		os.environ["TTR_PLAYCOOKIE"] = r.json()[u'cookie']
+		os.environ["TTR_GAMESERVER"] = r.json()[u'gameserver']
+		if vb:
+			gw = subprocess.Popen(args="TTREngine.exe")
+			spHandler(gw, tc, rs)
+		else:
+			gw = subprocess.Popen(args="TTREngine.exe", creationflags=0x08000000)
+		return True
+	else:
+		print("Oof! Login has failed. Press any key to restart...")
 		junk = msvcrt.getch()
 		os.system("cls")
 		login()
@@ -91,11 +131,12 @@ def debugStart():
 	os.environ["TT_GAMESERVER"] = "gs.corporateclash.net"
 	clash = subprocess.Popen(args="CorporateClash.exe")
 	
-def spHandler(clash, tc, rs):
+def spHandler(gw, tc, rs):
 	import time
+	print("Please note that verbose output only works for the last client opened.")
 	while True:
 		time.sleep(3)
-		poll = clash.poll()
+		poll = gw.poll()
 		if poll != None:
 			if rs:
 				startgame(tc, True, True)
@@ -157,8 +198,16 @@ game = ''
 p = os.path.split(os.path.normpath(sys.path[0]))[1]
 if p == "Corporate Clash":
 	game = 'C'
+elif p == "Toontown Rewritten":
+	if shell.IsUserAnAdmin():
+		game = 'R'
+	else:
+		print("This script must be run as an administrator in order to launch Toontown Rewritten. Press any key to exit...")
+		junk = msvcrt.getch()
+		os.system("cls")
+		sys.exit()
 else:
-	print("Please put this file in your Corporate Clash folder! Press any key to exit...")
+	print("Please put this file in your Corporate Clash or Toontown Rewritten folder. Press any key to exit...")
 	junk = msvcrt.getch()
 	os.system("cls")
 	sys.exit()
